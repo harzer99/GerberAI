686219d174d5385735867e655dc5b061ead1bdbe
harzer99 <96850723+harzer99@users.noreply.github.com>
2023-03-30 17:28:13 +0200

    restructured folder

--- youtubescraper/audio_scraper.py
+++ youtubescraper/audio_scraper.py
@@ -5,19 +5,6 @@ import os
 from tqdm import tqdm
 from multiprocessing.pool import Pool
 
-
-class Worker:
-    def __init__(self, worker, n):
-        self.n = n
-        self.worker = worker
-
-    def __len__(self):
-        return self.n
-
-    def __iter__(self):
-        return next(worker)
-
-
 def single_track_download(video):
     try:
         stream = video.streams.filter(only_audio=True).order_by('abr').last()   #get the audio stream with the highest bitrate
@@ -59,18 +46,25 @@ class audio_scraper():
         for filename in tqdm(os.listdir(self.track_directory), desc='rewriting to trainingdata'):
             path = os.path.join(self.track_directory, filename)
             y, sr = librosa.load(path)
+            y = librosa.to_mono(y)
             tempo, beats = librosa.beat.beat_track(y=y, sr=sr, start_bpm = 120)
+            beats = librosa.frames_to_time(beats, sr=sr)
+            beats = (beats*sr).astype(int)
             beats = np.delete(beats, np.where(beats < self.training_window*sr))     #getting rid of the beats that would result in training on parts of the previous track
-            audio = np.append(audio, y)
             flags = np.append(flags, beats + len(audio))
+            audio = np.append(audio, y)
+            print(np.max(flags), len(audio))
+            if sr != 22050:
+                print('differing samplerate')
         np.save(os.path.join(self.output_directory, 'audio'), audio)
         np.save(os.path.join(self.output_directory, 'flags'), flags)
+        print(sr)
+
         
-#playlist_url = 'https://www.youtube.com/watch?v=3V_yKgv1UUU&list=PL1ntfXx-b2MyxKr-kRkpe0xxcqwB4fRLz&ab_channel=HATE'
-playlist_url = 'https://www.youtube.com/playlist?list=PLHZZwMs7hXSDFqLPM4q5CaUwy5r4B5CS7'
+playlist_url = ''
 output_path = 'training_data/raw_music'
 
 if __name__ == '__main__':
-    myaudioanalyzer = audio_scraper('training_data/raw_music', 'training_data', 6)
-    myaudioanalyzer.download(playlist_url)
+    myaudioanalyzer = audio_scraper('E:\musik\\trainingsmusic\ghettotech', 'E:\musik\\trainingsmusic', 6)
+    #myaudioanalyzer.download(playlist_url)
     myaudioanalyzer.analyze()
