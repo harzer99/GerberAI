d6e339202f81f1d184b5935ca3fd3d34ce76659e
harzer99 <96850723+harzer99@users.noreply.github.com>
2023-04-06 18:07:23 +0200

    Update video_download_analzye.py

--- youtubescraper/video_download_analzye.py
+++ youtubescraper/video_download_analzye.py
@@ -28,10 +28,51 @@ def single_video_download(video_url):
     except:
         print('skipping song')
 
+
 def export_frame(beat, sr, vidfile, audio):
     seek = beat/sr
     image_path = f'{image_directory}\\{beat + len(audio)}.png'
     subprocess.run(['ffmpeg', '-hide_banner', '-nostats', '-y', '-ss', f'{seek:.2f}', '-i', vidfile, '-frames:v', '1', image_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL);
+
+
+#exporting frames and audio snippeds for the pretrained GAN
+def export_for_imgen(audio_file):
+    audio_path = audio_directory + '\\'+ audio_file
+    id = audio_path.split('\\')[-1]
+    id = id[0:-5]
+    video_path = video_directory +'\\' + id + '.mp4'
+    id_vid = video_path.split('\\')[-1]
+    id_vid = id_vid[0:-4]
+    assert id == id_vid
+
+    if not os.path.isfile(video_path):
+        print('no vid to this audio')
+        return
+
+    #getting length of video
+    args= ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', audio_path]
+    result = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    length = float(result.stdout)
+    if length > 600:
+        print('skipped track due to being more than 10min')
+        return
+    
+    # running through each video and the corresponding audio to create snippets
+    i = 0
+    seeks = np.linspace(start = 10, stop = length-10, num = 20)
+    for seek in seeks:
+        path = snippet_directory + '\\'+ id + '_'+ str(i).zfill(3)
+        image_path = path + '.png'
+        snipped_path = path + '.wav'
+        CMD_audio = ['ffmpeg', '-hide_banner', '-nostats', '-y', '-ss', f'{seek-5:.2f}', '-i', audio_path, '-t', '5', '-ar', '44100',  snipped_path]
+        CMD_image = ['ffmpeg', '-hide_banner', '-nostats', '-y', '-ss', f'{seek:.2f}', '-i', video_path, '-frames:v', '1', image_path]
+        subprocess.run(CMD_audio, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL);
+        subprocess.run(CMD_image, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL);
+        if os.path.getsize(snipped_path) < 1000:
+            os.remove(image_path)
+            os.remove(snipped_path)
+            print('something went wrong with the audio, deleting file again')
+        i += 1
 
 class Video_Prepper():
     def __init__(self, video_directory, audio_directory, output_directory, training_window) -> None:
@@ -89,23 +130,48 @@ class Video_Prepper():
         np.save(os.path.join(self.output_directory, 'audio'), audio)
         np.save(os.path.join(self.output_directory, 'flags'), flags)
         print(sr)
-
-
-
+    
+    # exports snippeds from downloaded .mp4 and corresponding .webm files from youtube downlaods
+    def export_snippets(self):
+        audio_files = os.listdir(self.audio_directory)    
+        n = len(audio_files)
+        print('Number of Songs: {:.2f}'.format(n))
+        i = 0
+        pool = Pool()
+        worker = pool.imap_unordered(export_for_imgen, audio_files[:n])
+        with tqdm(total=n) as pbar:
+            while i < n:
+                next(worker)
+                pbar.update()
+                i += 1
+        pool.close() # Close the pool to prevent any more tasks from being submitted
+        pool.join()
+        print('finished')
+        
+###########################################################
 #these two paths have to be added. It will create 
-playlist_file = 'videolists\\from_tim.txt'
-main_directory = 'E:\musik\\trainingsmusic'
+###########################################################
 
+playlist_file = 'youtubescraper\\videolists\\hate_podcast.txt'
+main_directory = 'C:\\GerberAI'
+
+
+###########################################################
 #creating all the directories
-video_directory = main_directory +'\downloaded_videos'
-audio_directory = main_directory +'\downloaded_audios'
-image_directory = main_directory +'\images'
+video_directory = main_directory +'\\downloaded_videos'
+audio_directory = main_directory +'\\downloaded_audios'
+image_directory = main_directory + '\\images'
+snippet_directory = main_directory +'\\snippets'
+imgen_output = main_directory +'\\imggen_output'
+
 os.makedirs(video_directory, exist_ok = True )
 os.makedirs(audio_directory, exist_ok = True)
 os.makedirs(image_directory, exist_ok = True)
+os.makedirs(snippet_directory, exist_ok = True)
 
 
 if __name__ == '__main__':
     myprepper = Video_Prepper(video_directory, audio_directory, main_directory, 6)
-    #myprepper.download(playlist_file)
-    myprepper.analyze()
+    myprepper.download(playlist_file)
+    #myprepper.analyze()
+    myprepper.export_snippets()
